f1d54524f93223305ea78129045f42775280e73d
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-08-21 17:18:14 +0300

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :hero_powers, only: [:show, :create] do
-    resources :heroes, only: :show
-  end
+  resources :hero_powers, only: [:create]
   resources :powers, only: [:index, :show, :update]
   resources :heroes, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
