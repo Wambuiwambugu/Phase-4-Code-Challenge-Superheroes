65100108e34b257a320fb143af8b3bf35b28bc8c
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-08-21 13:24:53 +0300

    update heroes routes

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,2 +1,23 @@
 class HeroesController < ApplicationController
+    def index
+        heroes = Hero.all
+        render json: heroes, except: excluded_attributes, status: :ok
+    end
+
+    def show
+        hero = Hero.find_by(id: params[:id])
+        if hero
+            render json: hero.as_json(
+                except: excluded_attributes,
+                include: {powers: {except: excluded_attributes}}
+            ), status: :ok
+        else
+            render json: {error: "Hero not found"}, status: :not_found
+        end
+    end
+
+    private
+    def excluded_attributes
+        [:created_at, :updated_at]
+    end
 end
